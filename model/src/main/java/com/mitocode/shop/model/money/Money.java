4fc9064653b28be23a517a0dd489a3929ced3d63
jalbertxd <jalbertxd@gmail.com>
2024-10-17 22:35:57 -0500

    Initial commit

---
+++ model/src/main/java/com/mitocode/shop/model/money/Money.java
@@ -0,0 +1,19 @@
+package com.mitocode.shop.model.money;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Objects;
+
+public record Money(Currency currency, BigDecimal amount) {
+    public Money{
+        Objects.requireNonNull(currency, "'currency' must not be null");
+        Objects.requireNonNull(amount, "'amount' must not be null");
+        if (amount.scale() > currency.getDefaultFractionDigits()){
+            throw new IllegalArgumentException(
+                    "Scale of amount %s is greater than the number of fraction digits used with the currency %s"
+                            .formatted(amount, currency));
+        }
+    }
+
+
+}
